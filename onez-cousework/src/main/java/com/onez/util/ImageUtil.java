a27297a561c637ada4087a8be1a42c56d7fcd69b
ASUS <ASUS@Abhinav>
2025-05-02 14:14:17 +0545

    Dashboard Heading

--- onez-cousework/src/main/java/com/onez/util/ImageUtil.java
+++ onez-cousework/src/main/java/com/onez/util/ImageUtil.java
@@ -96,6 +96,6 @@ public class ImageUtil {
 	}
 	
 	public String getSavePath(String saveFolder) {
-		return "C:/Users/IP3/eclipse-workspace/onez-coursework/src/main/webapp/resources/"+saveFolder+"/";
+		return "C:/Users/ASUS/git/onez/onez-coursework/src/main/webapp/resources/"+saveFolder+"/";
 	}
 }
