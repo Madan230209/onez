a25629f1db745c115dbcd5c67d49414a7bfedf4b
Pratyush Badal <pratyushbadal90@gmail.com>
2025-05-05 18:16:10 +0545

    Image Util path

--- onez-cousework/src/main/java/com/onez/util/ImageUtil.java
+++ onez-cousework/src/main/java/com/onez/util/ImageUtil.java
@@ -77,7 +77,7 @@ public class ImageUtil {
 	    saveToLocation(part, deployPath);
 	    
 	    // 2. Save to project directory (for easy access in Eclipse)
-	    String projectPath = "C:/Users/IP3/git/onez/onez-cousework/src/main/webapp/resources/" + saveFolder;
+	    String projectPath = "C:/Users/Acer/git/onez/onez-cousework/src/main/webapp/resources/" + saveFolder;
 	    saveToLocation(part, projectPath);
 	    
 	    return true;
